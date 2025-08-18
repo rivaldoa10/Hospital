1d88070f919bc8e69c90ab9812fdbb719c631506
rivaldoa10 <rivaldoaperez10@hotmail.com>
2025-08-18 11:51:11 -0600

    Corrigiendo ruta de Register

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,12 +1,10 @@
 import { Routes } from '@angular/router';
 import { Dashboard } from './pages/dashboard/dashboard';
 import { Notfound } from './pages/notfound/notfound';
-import path from 'path';
-import { Component } from '@angular/core';
 import { Pages } from './pages/pages';
-import { register } from 'module';
 import { Login } from './auth/login/login';
-import { Register } from './pages/register/register';
+import { Register } from './auth/register/register';
+
 
 export const routes: Routes = [
     {path:"", component: Pages, 
